d99f0316218ca3b2d439cca7ed6664f4f2842e2c
nguetcheu <dnguetcheu@gmail.com>
2024-09-28 09:26:32 +0200

    Ajout du style pour affichage des livres

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -12,7 +12,7 @@ export class BooksComponent implements OnInit {
       name: 'Clean code',
       author: 'Robert C martin',
       src: 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg',
-      amount: 7,
+      amount: 70,
     },
     {
       name: 'The Pragmatic Programmer',
@@ -21,10 +21,16 @@ export class BooksComponent implements OnInit {
       amount: 21,
     },
     {
-      name: 'Art of Computer Programming, The, Volumes 1-4B',
+      name: 'Art of Computer Programming collection',
       author: 'Donald Knuth ',
       src: 'https://m.media-amazon.com/images/I/719SYGJejmL._SY342_.jpg',
       amount: 261,
+    },
+    {
+      name: 'Introduction to Algorithms, fourth edition',
+      author: 'Thomas H. Cormen  ',
+      src: 'https://m.media-amazon.com/images/I/61Mw06x2XcL._SY342_.jpg',
+      amount: 88,
     },
   ];
 
