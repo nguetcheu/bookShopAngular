508094a93cecc7f1a7502a8451a2ccc5855d23c3
nguetcheu <dnguetcheu@gmail.com>
2024-09-28 10:28:09 +0200

    usage de la fonction getBook() depuis le service booksService

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../interface/Book';
+import { BooksService } from './books.service';
 
 @Component({
   selector: 'app-books',
@@ -7,40 +8,17 @@ import { Book } from '../interface/Book';
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
-  books: Book[] = [
-    {
-      name: 'Clean code',
-      author: 'Robert C martin',
-      src: 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg',
-      amount: 70,
-    },
-    {
-      name: 'The Pragmatic Programmer',
-      author: 'David Thomas',
-      src: 'https://m.media-amazon.com/images/I/91WFb-PpoNL._SX342_.jpg',
-      amount: 21,
-    },
-    {
-      name: 'Art of Computer Programming collection',
-      author: 'Donald Knuth ',
-      src: 'https://m.media-amazon.com/images/I/719SYGJejmL._SY342_.jpg',
-      amount: 261,
-    },
-    {
-      name: 'Introduction to Algorithms, fourth edition',
-      author: 'Thomas H. Cormen  ',
-      src: 'https://m.media-amazon.com/images/I/61Mw06x2XcL._SY342_.jpg',
-      amount: 88,
-    },
-  ];
+  books: Book[] = [];
 
   card: Book[] = [];
 
   isShowing: boolean = true;
 
-  constructor() {}
+  constructor(private bookService: BooksService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.books = this.bookService.getBook();
+  }
 
   addToCard(newBook: Book) {
     console.log(newBook);
--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../interface/Book';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BooksService {
+  constructor() {}
 
-  constructor() { }
+  getBook(): Book[] {
+    return [
+      {
+        name: 'Clean code',
+        author: 'Robert C martin',
+        src: 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg',
+        amount: 70,
+      },
+      {
+        name: 'The Pragmatic Programmer',
+        author: 'David Thomas',
+        src: 'https://m.media-amazon.com/images/I/91WFb-PpoNL._SX342_.jpg',
+        amount: 21,
+      },
+      {
+        name: 'Art of Computer Programming collection',
+        author: 'Donald Knuth ',
+        src: 'https://m.media-amazon.com/images/I/719SYGJejmL._SY342_.jpg',
+        amount: 261,
+      },
+      {
+        name: 'Introduction to Algorithms, fourth edition',
+        author: 'Thomas H. Cormen  ',
+        src: 'https://m.media-amazon.com/images/I/61Mw06x2XcL._SY342_.jpg',
+        amount: 88,
+      },
+    ];
+  }
 }
