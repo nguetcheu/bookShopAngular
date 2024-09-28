1ef73cc55bf9038bee38d4e2b6ef04a4031bdc2c
nguetcheu <dnguetcheu@gmail.com>
2024-09-28 10:37:40 +0200

    Création de booksModule et exportation de se module pour etre utilisable partout au sein de l'application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './books/books.component';
 import { FormsModule } from '@angular/forms';
-import { BookComponent } from './book/book.component';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent, BookComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BooksModule],
   providers: [],
   bootstrap: [AppComponent],
 })
